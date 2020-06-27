9e2926056bb5531b0063a843cbc08b1163ee85c5
Denis Khrapov <khrapov@haulmont.com>
2020-06-27 14:52:27 +0400

    added work with certificates

--- src/main/java/com/alfa/battle/BattleApplication.java
+++ src/main/java/com/alfa/battle/BattleApplication.java
@@ -1,13 +1,68 @@
 package com.alfa.battle;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class BattleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BattleApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			KeyStore trustStore;
+			HttpComponentsClientHttpRequestFactory requestFactory = null;
+
+			trustStore = KeyStore.getInstance("JKS");
+			ClassPathResource classPathResource = new ClassPathResource("client_trustore.jks");
+			InputStream inputStream = classPathResource.getInputStream();
+			trustStore.load(inputStream, "123456".toCharArray());
+
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			ClassPathResource classPathResourceKeyStore = new ClassPathResource("client.jks");
+			InputStream inputStreamKeyStore = classPathResourceKeyStore.getInputStream();
+			keyStore.load(inputStreamKeyStore, "123456".toCharArray());
+
+			SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(new SSLContextBuilder()
+					.loadTrustMaterial(trustStore, new TrustSelfSignedStrategy())
+					.loadKeyMaterial(keyStore, "123456".toCharArray())
+					.build());
+
+			CloseableHttpClient httpClient = HttpClients.custom()
+					.setSSLSocketFactory(socketFactory)
+					.setMaxConnTotal(Integer.valueOf(5))
+					.setMaxConnPerRoute(Integer.valueOf(5))
+					.build();
+
+			requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+			requestFactory.setReadTimeout(Integer.valueOf(10000));
+			requestFactory.setReadTimeout(Integer.valueOf(10000));
+
+			restTemplate.setRequestFactory(requestFactory);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return restTemplate;
 	}
 
 }
--- src/main/java/com/alfa/battle/controllers/ATMFinderController.java
+++ src/main/java/com/alfa/battle/controllers/ATMFinderController.java
@@ -1,13 +1,18 @@
 package com.alfa.battle.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ATMFinderController {
 
-    @RequestMapping("/find")
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/showAll")
     public String findATM() {
-        return "There is no ATM";
+        return restTemplate.getForEntity("https://apiws.alfabank.ru/alfabank/alfadevportal/atm-service/atms/status", String.class).getBody();
     }
 }
